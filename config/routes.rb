18f4e2f16cb27eb28b2f501ecf5d28cd481d88a0
stephane martin <stephane@s-martin.fr>
2026-05-19 14:57:15 +0200

    architecture

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,19 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+
+  root "tasks#index"
+
+  get    "tasks",          to: "tasks#index",   as: :tasks
+  get    "tasks/new",      to: "tasks#new",     as: :new_task
+  post   "tasks",          to: "tasks#create"
+
+  get    "tasks/:id",      to: "tasks#show",    as: :task
+  get    "tasks/:id/edit", to: "tasks#edit",    as: :edit_task
+  patch  "tasks/:id",      to: "tasks#update"
+  put    "tasks/:id",      to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
